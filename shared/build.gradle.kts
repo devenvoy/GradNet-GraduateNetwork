1505c60815839204c27baff002b66d065846a226
devenvoy <iambeast0069@gmail.com>
2025-02-19 01:59:08 +0530

    ``` feat: set up Kotlin Multiplatform for iOS and Android
    This commit introduces the setup for Kotlin Multiplatform to support both iOS and Android platforms.
    
    ``````
    - Created a `shared` module using Kotlin Multiplatform plugin.
    - Configured `iosX64`, `iosArm64`, and `iosSimulatorArm64` as targets.
    - Added `compose` related dependencies in common source set.
    - Added `content.xcworkspacedata` in ios app.
    - Added `Pod-iosApp` in ios project.
    - updated ios app project to use pods.
    - configured framework binaries to be static.
    - Added configurations for `compileSdk`, `minSdk` and `namespace`.
    - configured compose resources and added `resources` class.
    ```

---
+++ shared/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+
+    kotlin("multiplatform")
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+
+        commonMain.dependencies {
+            implementation(compose.ui)
+            implementation(compose.runtime)
+            implementation(compose.material3)
+            implementation(compose.foundation)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+
+        }
+    }
+}
+
+android {
+    namespace = "com.sdjic.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+compose.resources {
+    publicResClass = true
+    packageOfResClass = "com.sdjic.shared.resources"
+    generateResClass = auto
+}
+
+
+dependencies {
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.ui.android)
+}

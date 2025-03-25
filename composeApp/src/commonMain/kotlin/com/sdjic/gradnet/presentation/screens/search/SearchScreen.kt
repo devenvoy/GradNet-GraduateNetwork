3de163658ff1fa18870df2b398d095b41311d74f
devenvoy <iambeast0069@gmail.com>
2025-03-25 15:35:02 +0530

    feat: Enhance Post and Search Screens with loading indicators and refresh
    This commit improves the Post and Search screens by adding loading indicators and pull-to-refresh functionality.
    
    -   **Post Screen**:
        - Added a loading indicator for the initial load state.
        - The indicator is now correctly triggered only once at the start.
        - Removed unused code.
    -   **Search Screen**:
        - Implemented pull-to-refresh for loading users.
        - Added a loading indicator for refreshing.
        - Ensured that the loading state is triggered correctly.
    - Updated `UserItem` composable to set the height and add `SpaceEvenly` to the arrangement to improve UI.
    - Added maxLines and ellipsis to UserItem to fix long user name texts.

--- composeApp/src/commonMain/kotlin/com/sdjic/gradnet/presentation/screens/search/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sdjic/gradnet/presentation/screens/search/SearchScreen.kt
@@ -1,8 +1,8 @@
 package com.sdjic.gradnet.presentation.screens.search
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,15 +19,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import app.cash.paging.compose.collectAsLazyPagingItems
 import cafe.adriel.voyager.core.screen.Screen
 import com.sdjic.gradnet.data.network.entity.dto.UserDto
@@ -51,17 +59,32 @@ class SearchScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun SearchScreenContent() {
-        val navigator = LocalRootNavigator.current
-
         val scope = rememberCoroutineScope()
-        val navigateToDetail: (String) -> Unit = { navigator.push(ProfileScreen(it)) }
-        val viewModel = koinScreenModel<SearchScreenModel>()
+        val pullToRefreshState = rememberPullToRefreshState()
+        val navigator = LocalRootNavigator.current
         val scrollBehavior = LocalScrollBehavior.current
+        val viewModel = koinScreenModel<SearchScreenModel>()
+
+        val navigateToDetail: (String) -> Unit = { navigator.push(ProfileScreen(it)) }
 
         val query by viewModel.query.collectAsState()
         val isActive by viewModel.searchActive.collectAsState()
-
         val data = viewModel.users.collectAsLazyPagingItems()
+
+        var hasLoadedOnce by remember { mutableStateOf(false) }
+
+        val isRefreshing by remember {
+            derivedStateOf {
+                if (!hasLoadedOnce) false
+                else data.loadState.refresh is LoadState.Loading
+            }
+        }
+
+        LaunchedEffect(data.loadState) {
+            if (data.loadState.refresh is LoadState.NotLoading) {
+                hasLoadedOnce = true
+            }
+        }
 
         Scaffold {
 
@@ -85,14 +108,20 @@ class SearchScreen : Screen {
                     }
                 )
 
-                CashPagingGridUi(
-                    paddingValues = PaddingValues(10.dp),
-                    data = data,
-                    state = rememberLazyGridState()
-                ) { item, modifier ->
-                    item?.let {
-                        UserItem(item) {
-                            navigateToDetail(item.userId)
+                PullToRefreshBox(
+                    isRefreshing = isRefreshing,
+                    state = pullToRefreshState,
+                    onRefresh = { data.refresh() }
+                ) {
+                    CashPagingGridUi(
+                        paddingValues = PaddingValues(10.dp),
+                        data = data,
+                        state = rememberLazyGridState()
+                    ) { item, modifier ->
+                        item?.let {
+                            UserItem(item) {
+                                navigateToDetail(item.userId)
+                            }
                         }
                     }
                 }
@@ -108,19 +137,22 @@ class SearchScreen : Screen {
             modifier = Modifier
                 .padding(4.dp)
                 .fillMaxWidth()
+                .height(200.dp)
         ) {
             Column(
                 modifier = Modifier.padding(8.dp).fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 CircularProfileImage(
                     placeHolderName = user.username,
                     data = user.profilePic
                 )
-                Spacer(modifier = Modifier.height(8.dp))
                 Text(
                     modifier = Modifier.fillMaxWidth(),
                     textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
                     fontFamily = displayFontFamily(),
                     text = user.username, style = MaterialTheme.typography.bodyLarge
                 )
